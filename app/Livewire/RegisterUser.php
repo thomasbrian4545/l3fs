d85693c7537ceec599f3c36646563b6af2ca0f4d
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-11-04 21:33:14 +0700

    Forms

---
+++ app/Livewire/RegisterUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+
+class RegisterUser extends Component
+{
+    #[Rule("required")]
+    public $name;
+    #[Rule("required|unique:users|email")]
+    public $email;
+    #[Rule("required|min:5")]
+    public $password;
+
+    public function render()
+    {
+        return view('livewire.register-user');
+    }
+
+    public function save()
+    {
+        $this->validate();
+        dd(123);
+    }
+}
